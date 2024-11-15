bdb6bd1ee0fed0c9b1df34180de3262a2aaea55e
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-11-15 16:00:00 +0000

    Added Footer component with a paragraph of text and a link to GitHub profile

---
+++ Files Converter/Project N2/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p>Made with <span style={{ color: 'red' }}>❤</span>,  by <a href="https://github.com/y1hy11" target="_blank">y1hy11</a></p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
