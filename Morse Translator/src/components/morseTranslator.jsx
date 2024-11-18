1a3fa4cd7be01b574af780afb8abc9390bc7ee49
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-11-18 20:15:48 +0000

    Implement Morse Code Translator component with input handling and translation logic

---
+++ Morse Translator/src/components/morseTranslator.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import morseCode from './morseCode';
+
+const MorseTranslator = () => {
+    const [inputText, setInputText] = useState('');
+    const [morseText, setMorseText] = useState('');
+
+    const translateToMorse = (text) => {
+        return text.toUpperCase().split('').map(char => morseCode[char] || '').join(' ');
+    };
+
+    const handleTranslate = () => {
+        const translated = translateToMorse(inputText);
+        setMorseText(translated);
+    };
+
+    return (
+        <div>
+            <div>
+                <h1>Text to Morse Code Translator</h1>
+                <div>
+                    <textarea
+                        rows="4"
+                        cols="50"
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                        placeholder="Type your text here..."
+                    />
+                </div>
+                <div >
+                    <button onClick={handleTranslate}>Translate to Morse</button>
+                </div>
+                <h2>Morse Code:</h2>
+                <div>{morseText}</div>
+                <div>
+                     <p>Made with <span style={{ color: 'red' }}>❤</span>,  by <a href="https://github.com/y1hy11" target="_blank">y1hy11</a></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MorseTranslator;
